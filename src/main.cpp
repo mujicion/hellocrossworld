d0804fe2161ae2a6b216722318c14cdefa10a284
mujicion <mujicion@gmail.com>
2025-02-07 03:45:37 +0000

    STL string vector iterator 사용

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+std::string hello() {
+    return "Hello";
+}
+
+std::string world() {
+    return "World";
+}
+
+int main() {
+    std::vector<std::string> messages;
+    messages.push_back(hello());
+    messages.push_back(world());
+
+    for (auto it = messages.begin(); it != messages.end(); ++it) {
+        std::cout << *it;
+        if (it + 1 != messages.end()) {
+            std::cout << " ";
+        }
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
